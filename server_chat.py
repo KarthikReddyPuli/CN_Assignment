9aa80cd449402a785fd3e193dd1d0df3ef97803b
Spurthi31 <71369962+Spurthi31@users.noreply.github.com>
2020-12-17 23:19:13 +0530

    Update server_chat.py

--- server_chat.py
+++ server_chat.py
@@ -8,60 +8,61 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 #Check for the proper arguments
 if len(sys.argv) != 3:
-	print ("Correct usage: script, IP address, port number")
+	print ("Arguments format: <script>, <IP address>, <port number>")
 	exit()
 
 
-IP_address = str(sys.argv[1])
-Port = int(sys.argv[2])
+IP_addr = str(sys.argv[1])
+port_num = int(sys.argv[2])
 
 #Binding the socket to the specified IP and port
-server.bind((IP_address, Port))
-server.listen(100)  #Accepts maximum of 100 clients
+server.bind((IP_addr, port_num))
+server.listen(50)  #Accepts maximum of 50 clients
 
-list_of_clients = [] #All the list of clients are stored here for futher use to broadcast
+#All the list of clients are stored in clients_list for futher use to broadcast
+clients_list = [] 
 
-def clientthread(conn, addr): #Receives all the messages from a specific client and sends to all other clients
-	# sends a message to the client whose user object is conn 
-	conn.send("Welcome to this chatroom!".encode())
+def clientthread(con, address): #Receives all the messages from a specific client and sends to all other clients
+	# message is sent to client. Client user object is con
+	con.send("Successfully connected to chatroom".encode())
 	while True:
 			try:
-				message = conn.recv(2048).decode()
-				if message:
-					print ("<" + addr[0] + "> " + message)
-					# Calls broadcast function to send message to all 
-					message_to_send = "<" + addr[0] + "> " + message 
-					broadcast(message_to_send, conn)
+				msg = con.recv(2048).decode()
+				if msg:
+					print ("<" + address[0] + "> " + msg)
+					# message is sent to all using broadcast function 
+					send_msg = "<" + address[0] + "> " + msg 
+					broadcast(send_msg, con)
 				else: #This occurs when the client program or the socket stops working
-					remove(conn)
+					remove(con)
 
 			except: #Error Handler
 				continue
 
 
-
-def broadcast(message, connection):
-	for clients in list_of_clients:
-		if clients!=connection:
+# message is sent to all clients using broadcast function 
+def broadcast(msg, con):
+	for clients in clients_list:
+		if clients!=con:
 			try:
-				clients.send(message.encode())
+				clients.send(msg.encode())
 			except:
 				clients.close()
-				# if the link is broken, we remove the client 
+				# remove client if link is broken
 				remove(clients)
 
-
+#removes the object 
 def remove(connection):
-	if connection in list_of_clients:
-		list_of_clients.remove(connection)
+	if connection in clients_list:
+		clients_list.remove(connection)
 
 while True:
 
-	conn, addr = server.accept()
-	list_of_clients.append(conn) #Adds the client to list of all clients
-	client_thread=threading.Thread(target = clientthread,args = (conn,addr,))
+	con, address = server.accept()
+	clients_list.append(con) #Adds the client to list of all clients
+	client_thread=threading.Thread(target = clientthread,args = (con,address,))
 	client_thread.start() #Starts a new thread for every client
-	print (str(addr[0]) + ":" + str(addr[1]) + " connected")
+	print (str(address[0]) + ":" + str(address[1]) + " connected")
 
-conn.close()
+con.close()
 server.close()
