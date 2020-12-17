a42a698a0af210f25cb8c0fb23f46441180d4a68
nishantr05 <66346272+nishantr05@users.noreply.github.com>
2020-12-17 18:11:41 +0530

    Update server_ssl.py

--- server_ssl.py
+++ server_ssl.py
@@ -1,35 +1,49 @@
-#!/bin/usr/env python
 import socket
 import ssl
+import sys
 import pprint
 
-#server
-if __name__ == '__main__':
+#defining ip address and port values
+ip_addr, port = '127.0.0.1', 42420
 
-    HOST = '127.0.0.1'
-    PORT = 1234
+###---- Setting up the server socket ----##
+# Creating a socket object, 'ssl_serversoc'.
+# 'AF_NET'-> Ipv4 addressing, 'SOCK_STREAM'-> for TCP protocol.
+ssl_serversoc = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+ssl_serversoc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen(10)
+#Binding the socket:
+try:
+    ssl_serversoc.bind((ip_addr, port))       
+    print ("Socket binded to %s" %(port))  
+except:
+    print("Failed to bind socket. Error:" + str(sys.exc_info()))
+    sys.exit()
 
-    client, fromaddr = server_socket.accept()
-    secure_sock = ssl.wrap_socket(client, server_side=True, ca_certs = "client.pem", certfile="server.pem", keyfile="server.key", cert_reqs=ssl.CERT_REQUIRED,
-                           ssl_version=ssl.PROTOCOL_TLSv1_2)
+# Setting max queue size of requests
+ssl_serversoc.listen(7)
 
-    print(repr(secure_sock.getpeername()))
-    print(secure_sock.cipher())
-    print(pprint.pformat(secure_sock.getpeercert()))
-    cert = secure_sock.getpeercert()
-    print(cert)
+client, fromaddr = ssl_serversoc.accept()
+# Configuring secure SSL connection
+secure_sock = ssl.wrap_socket(client, server_side=True, ca_certs = "client.pem", certfile="server.pem", keyfile="server.key", cert_reqs=ssl.CERT_REQUIRED,
+                        ssl_version=ssl.PROTOCOL_TLSv1_2)
 
-    # verify client
-    #if not cert or ('commonName', 'test') not in cert['subject'][3]: raise Exception("ERROR")
+print("-----------------CLIENT INFO-----------------")
 
-    try:
-        data = secure_sock.read(1024)
-        secure_sock.write(data)
-    finally:
-        secure_sock.close()
-        server_socket.close()
+print(repr(secure_sock.getpeername()))
+#print(secure_sock.cipher())
+print(pprint.pformat(secure_sock.getpeercert()))
+cert = secure_sock.getpeercert()
+print(cert)
+print("-----------------END-----------------")
+
+#ECHO Server implementation
+
+print("\nReceiving data....")
+try:
+    data = secure_sock.read(1024)
+    print(f"Data recieved -> {data.decode()}")
+    secure_sock.write(data)
+finally:
+    secure_sock.close()
+    ssl_serversoc.close()
