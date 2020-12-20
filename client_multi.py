ec0a8853b723885cc7253098cf5307bb6b919791
nishantr05 <66346272+nishantr05@users.noreply.github.com>
2020-12-20 09:42:37 +0530

    Update client_multi.py

--- client_multi.py
+++ client_multi.py
@@ -1,33 +1,41 @@
-MYPORT = 8123
-MYGROUP_4 = '225.0.0.250'
-MYGROUP_6 = 'ff15:7079:7468:6f6e:6465:6d6f:6d63:6173'
-MYTTL = 1 # Increase to reach other networks
-
 import time
 import struct
 import socket
 import sys
 
-def main():
-    group = MYGROUP_6 if "-6" in sys.argv[1:] else MYGROUP_4
-    sender(group)
+## Defining required values
+port = 42420
+multicast_group4, multicast_group6 = '224.3.1.1', 'ff08:6069:6069:6069:6069:6069:6069:6069'
+# ttl set to '1' to restrict connection to local network.
+# Can be increased to reach other networks
+ttl = 1 
 
-def sender(group):
-    addrinfo = socket.getaddrinfo(group, None)[0]
+if len(sys.argv) == 0:
+    print("Please give the  \'-4\' or \'-6\' argument ")
+    sys.exit()
+if sys.argv[1] == "-6":
+    group = multicast_group6
+elif sys.argv[1] == "-4":
+    group = multicast_group4
+else:
+    print("Enter valid argument (either \'-4\' or \'-6\')")
+    sys.exit()
 
-    s = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
 
-    # Set Time-to-live (optional)
-    ttl_bin = struct.pack('@i', MYTTL)
-    if addrinfo[0] == socket.AF_INET: # IPv4
-        s.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
-    else:
-        s.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
+#storing address info of the group
+addrinfo = socket.getaddrinfo(group, None)[0]
 
-    while True:
-        data = repr(time.time())
-        s.sendto((data + '\0').encode(), (addrinfo[4][0], MYPORT))
-        time.sleep(1)
+soc_obj = socket.socket(addrinfo[0], socket.SOCK_DGRAM)
 
-if __name__ == '__main__':
-    main()
+# Configuring group info
+ttl_bin = struct.pack('@i', ttl)
+if addrinfo[0] == socket.AF_INET: # IPv4
+    soc_obj.setsockopt(socket.IPPROTO_IP, socket.IP_MULTICAST_TTL, ttl_bin)
+else:
+    soc_obj.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_MULTICAST_HOPS, ttl_bin)
+
+print("Sending data to server!")
+while True:
+    data = "Test msg!"
+    soc_obj.sendto((data).encode(), (addrinfo[4][0], port))
+    time.sleep(1)
